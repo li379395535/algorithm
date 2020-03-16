e8a5fbe31cda9e9256e580fdcb9ea8aa4db9ee7f
Lee <379395535@QQ.com>
2020-03-16 23:24:16 +0800

    style

---
+++ src/courses/LRU/config.ts
@@ -0,0 +1,51 @@
+export const Cities = [
+  '上海',
+  '杭州',
+  '广州',
+];
+
+export const JobInfos = [
+  {
+    id: 1,
+    city: '上海',
+    position: '开发',
+    salary: 20000,
+    skill: 'c',
+  }, {
+    id: 2,
+    city: '杭州',
+    position: '开发',
+    salary: 20000,
+    skill: 'c',
+  }, {
+    id: 3,
+    city: '上海',
+    position: '开发',
+    salary: 20000,
+    skill: 'c',
+  }, {
+    id: 4,
+    city: '杭州',
+    position: '开发',
+    salary: 20000,
+    skill: 'c',
+  }, {
+    id: 5,
+    city: '杭州',
+    position: '开发',
+    salary: 20000,
+    skill: 'c',
+  }, {
+    id: 6,
+    city: '杭州',
+    position: '开发',
+    salary: 20000,
+    skill: 'c',
+  }, {
+    id: 7,
+    city: '广州',
+    position: '开发',
+    salary: 20000,
+    skill: 'c',
+  },
+];
--- src/courses/LRU/index.tsx
+++ src/courses/LRU/index.tsx
@@ -2,60 +2,10 @@ import React, { FunctionComponent, useState } from 'react';
 import { Select, Table } from 'antd';
 
 import LRUCache from './model/LRUCache';
+import { JobInfos, Cities } from "./config";
 interface LRUProps {
 }
 
-const Cities = [
-  '上海',
-  '杭州',
-  '广州',
-];
-
-const JobInfos = [
-  {
-    id: 1,
-    city: '上海',
-    position: '开发',
-    salary: 20000,
-    skill: 'c',
-  }, {
-    id: 2,
-    city: '杭州',
-    position: '开发',
-    salary: 20000,
-    skill: 'c',
-  }, {
-    id: 3,
-    city: '上海',
-    position: '开发',
-    salary: 20000,
-    skill: 'c',
-  }, {
-    id: 4,
-    city: '杭州',
-    position: '开发',
-    salary: 20000,
-    skill: 'c',
-  }, {
-    id: 5,
-    city: '杭州',
-    position: '开发',
-    salary: 20000,
-    skill: 'c',
-  }, {
-    id: 6,
-    city: '杭州',
-    position: '开发',
-    salary: 20000,
-    skill: 'c',
-  }, {
-    id: 7,
-    city: '广州',
-    position: '开发',
-    salary: 20000,
-    skill: 'c',
-  },
-];
 
 const { Option } = Select;
 const Cache = new LRUCache<Object[]>(10);
